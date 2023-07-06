4e14ab54a67e0aed5c3afa7f97bea587ec63f2af
Davis06 <davis06@outlook.com>
2023-07-06 02:00:22 +0100

    Established the store and defined cartSlice and uiSlice as separate state slices, incorporating reducer methods tailored to each slice.

---
+++ src/components/Cart/CartButton.js
@@ -0,0 +1,20 @@
+import { useDispatch, useSelector } from 'react-redux';
+import classes from './CartButton.module.css';
+import { uiActions } from '../../store/ui-slice';
+
+const CartButton = (props) => {
+  const dispatch = useDispatch()
+
+  const toggleCartHandler = () => {
+    dispatch(uiActions.toggle())
+  }
+
+  return (
+    <button className={classes.button} onClick={toggleCartHandler}>
+      <span>My Cart</span>
+      <span className={classes.badge}>1</span>
+    </button>
+  );
+};
+
+export default CartButton;
