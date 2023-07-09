44e03c543c13043add05be545704a19877c1e732
Davis06 <davis06@outlook.com>
2023-07-09 17:36:42 +0100

    Effectively rendered the cart component, ensuring the accurate display of cart contents. Additionally, implemented the functionality to adjust the quantity of items in the cart, allowing for both increasing and decreasing the item count.

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -14,7 +14,7 @@ const cartSlice = createSlice({
             state.totalQuantity++
             if (!existingItem) {
                 state.items.push({
-                    itemId: newItem.id,
+                    id: newItem.id,
                     price: newItem.price,
                     quantity: 1,
                     totalPrice: newItem.price,
@@ -30,9 +30,9 @@ const cartSlice = createSlice({
             const existingItem = state.items.find((item) => item.id === id)
             state.totalQuantity--
             if (existingItem.quantity === 1) {
-                state.items.filter((item) => item.id !== id)
+               state.items = state.items.filter((item) => item.id !== id)
             } else {
-                existingItem--
+                existingItem.quantity--
                 existingItem.totalPrice = existingItem.totalPrice - existingItem.price
             }
         }
