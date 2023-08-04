eae328e1e5f03fee54c34b4152b011a0d76dbe49
Davis06 <davis06@outlook.com>
2023-08-04 06:07:37 +0100

    Utilized the isInitial state to manage the useEffect hook, preventing it from executing and sending cart data to the backend on the initial render.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,81 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import { useEffect } from 'react';
+import { Fragment, useEffect, useState } from 'react';
+import { uiActions } from './store/ui-slice';
+import Notification from './components/UI/Notification';
+
+// let isInitial = true
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible)
-
+  const dispatch = useDispatch()
   const cart = useSelector((state) => state.cart)
+  const notification = useSelector((state) => state.ui.notification)
+  const [isInitial, setIsInitial] = useState(true)
 
   useEffect(() => {
-    fetch('https://redux-cart-3a2f0-default-rtdb.firebaseio.com/cart.json', {
-      method: 'PUT',
-      body: JSON.stringify(cart)
+    if (isInitial) {
+      setIsInitial(false)
+      return
+    }
+    const sendCartData = async () => {
+      dispatch(uiActions.showNotification({
+        status: 'pending',
+        title: 'Sending...',
+        message: 'Sending cart data!'
+      }))
+
+      const response = await fetch('https://redux-cart-3a2f0-default-rtdb.firebaseio.com/cart.json', 
+        {
+          method: 'PUT',
+          body: JSON.stringify(cart)
+        }
+      )
+
+      if (!response.ok) {
+        throw new Error('Sending cart data failed.')
+      }
+
+      dispatch(uiActions.showNotification({
+        status: 'success',
+        title: 'Success!',
+        message: 'Sent cart data successfully!'
+      }))
+
+    }
+
+    // if (isInitial) {  
+    //   isInitial = false
+    //   return;
+    // }
+
+    sendCartData().catch((error) => {
+      dispatch(uiActions.showNotification({
+        status: 'error',
+        title: 'Error',
+        message: 'Sending cart data failed!'
+      }))
     })
-  }, [cart])
+
+  }, [cart, dispatch])
 
   return (
-    <Layout>
-      { showCart &&
-        <Cart />
-      }
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification &&
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />}
+      <Layout>
+        {showCart &&
+          <Cart />
+        }
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -2,11 +2,18 @@ import { createSlice } from "@reduxjs/toolkit";
 
 
 const uiSlice = createSlice({
-    name: 'name',
-    initialState: { cartIsVisible: false },
+    name: 'ui',
+    initialState: { cartIsVisible: false, notification: null },
     reducers: {
         toggle (state) {
             state.cartIsVisible = !state.cartIsVisible
+        },
+        showNotification (state, action) {
+            state.notification = {
+                status: action.payload.status,
+                title: action.payload.title,
+                message: action.payload.message
+            }
         }
     }
 })
